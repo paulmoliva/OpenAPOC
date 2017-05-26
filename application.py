eaf793bdf2587118f1c55ead83f3164f220fe283
paulmoliva <paulmoliva@gmail.com>
2017-05-25 18:29:58 -0700

    add phones and van stuff to models

--- application.py
+++ application.py
@@ -552,6 +552,11 @@ class Voter(db.Model, BaseModel):
     contributor_id = db.Column(db.Integer, db.ForeignKey('contributors.id'), index=True, nullable=True)
     precinct = db.Column(db.Integer)
     PARTY = db.Column(db.String(255))
+
+    VANID = db.Column(db.Integer)
+    PreferredEmail = db.Column(db.String(255))
+    PreferredPhone = db.Column(db.String(255))
+
     # contributor = db.relationship(
     #     'Contributor',
     #     backref=db.backref('voter_contributions', lazy='dynamic')
